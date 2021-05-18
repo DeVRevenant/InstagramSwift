9cd94cd420f0b5f6c77f2ee0be4288bfc6646f45
DeVRevenant <79675081+DeVRevenant@users.noreply.github.com>
2021-05-18 14:24:47 +0100

    Add files via upload

---
+++ Instagram/Instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  Instagram
+//
+//  Created by itsector on 18/05/2021.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by itsector on 18/05/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
